b81a87b389dfdd4bae419264ad524b471b81c8e9
BloodDragon2580 <gewinner.thomas@gmail.com>
2020-11-07 18:00:21 +0100

    Update to 5.6
    Added Translation for zhTW. Thx to BNS333

---
+++ QuickChat/Locales/zhTW.lua
@@ -0,0 +1,19 @@
+if not QCL then QCL={}; end
+local tL={};
+
+tL["S"] = '說'
+tL["G"] = "公會"
+tL["P"] = "隊伍"
+tL["R"] = "團隊"
+tL["I"] = "副本"
+tL["RO"] = "擲骰"
+tL["RC"] = "團警"
+tL["PULL"] = "倒數"
+tL["BREAK"] = "休息10分"
+tL["RELOAD"] = "重載UI"
+tL["PARTY"] = "離開隊伍"
+tL["TITLE"] = "|cffe1a500 QuickChat Settings"
+tL["INFO"] = "|cffffffff Here you can check or uncheck the buttons,\ndepending on which buttons you want to use."
+tL["THX"] = "|cffff6060 A big Thx at KalitaAlexey for the settings Page."
+
+QCL.zhTW=tL;
